066569b8cb6e5de7e273f1f1ed817e0f1ff648bb
Tekin TOPUZ <tekintopuz@gmail.com>
2025-03-19 18:30:12 +0300

    update urls

--- core/settings.py
+++ core/settings.py
@@ -369,7 +369,7 @@ LOGGING = {
         'default': {
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'mylog.log',
+            'filename': 'logs/mylog.log',
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'standard',
@@ -377,7 +377,7 @@ LOGGING = {
         'request_handler': {
             'level': 'DEBUG',
             'class': 'logging.handlers.RotatingFileHandler',
-            'filename': 'django_request.log',
+            'filename': 'logs/django_request.log',
             'maxBytes': 1024 * 1024 * 5,  # 5 MB
             'backupCount': 5,
             'formatter': 'standard',
